0f1fbb0c6dd02afa582dd6986768a94e3ce3ac21
MMQ <sadiq.siddiqui@hotmail.com>
2023-10-19 10:01:36 +0500

    Initial Commit

---
+++ SwiftNwetworking/SwiftNwetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNwetworkingApp.swift
+//  SwiftNwetworking
+//
+//  Created by Muhammad on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNwetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
